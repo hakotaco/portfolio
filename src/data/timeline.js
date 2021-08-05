bd67dcb90369bbe8f992c704563375508e00c19f
AshDarkfold <ashutosh.kaushik2018@vitstudent.ac.in>
2021-08-05 17:07:45 +0530

    update: timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -2,28 +2,33 @@ const timelineData = [
   {
     line1: "12th standard",
     line2: "April 2018",
-    line3: "Salwan Public School"
+    line3: "Salwan Public School",
   },
   {
     line1: "B.tech (ECE)",
     line2: "July 2018 - May 2022",
-    line3: "Vellore Institute of Technology"
+    line3: "Vellore Institute of Technology",
   },
   {
     line1: "Frontend Intern",
     line2: "April 2020 - May 2020",
-    line3: "Kuzagan Tech. Pvt. Ltd."
+    line3: "Kuzagan Tech. Pvt. Ltd.",
   },
   {
-    line1: "Software Developer Intern",
+    line1: "Software Engineer Intern",
     line2: "May 2020 - July 2020",
-    line3: "Inqutec Labs Pvt. Ltd."
+    line3: "Inqutec Labs Pvt. Ltd.",
   },
   {
-    line1: "Fullstack Developer Intern",
+    line1: "Software Engineer Intern",
     line2: "December 2020 - April 2021",
-    line3: "Kuzagan Tech. Pvt. Ltd."
-  }
-]
+    line3: "Kuzagan Tech. Pvt. Ltd.",
+  },
+  {
+    line1: "Software Engineer Intern",
+    line2: "July 2021 - August 2021",
+    line3: "Loopclub Internet Private Limited",
+  },
+];
 
-export default timelineData
+export default timelineData;
