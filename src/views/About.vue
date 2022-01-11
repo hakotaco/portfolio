382c7c7f9d621e099e2c3a346c5b4dd65a681aeb
Ashutosh Kaushik <ashutosh@clearfeed.ai>
2022-01-11 20:01:20 +0530

    updated about me

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,11 +6,11 @@
       <main>
         <h1 class="animate__animated animate__fadeInUp">About me</h1>
         <p class="animate__animated animate__fadeInLeft">
-          A passionate developer with a dream. A dream to provide the best user experience out there. I’ve been consistently developing software with various companies, organisations, startups and clients since December 2019.
-          I’m proficient in JavaScript, both backend and frontend. Highly experienced in react and have a good hold of the fundamentals of Vue, Next JS and a bunch of commonly used libraries like bootstrap, ant-design etc.
+          A passionate developer with a dream to provide the best user experience out there. I’ve been consistently developing software with various companies, organisations, startups and clients since December 2019.
+          I’m proficient in TypeScript, both backend and frontend. Highly experienced in React and Next.js and have a good hold of the fundamentals of Vue and a bunch of commonly used libraries like bootstrap, ant-design etc.
         </p>
         <p class="animate__animated animate__fadeInLeft">
-          I write efficient REST API’s using Node.JS and Python (Django) and have a good amount of experience with relational (MySql, Postgresql) and non-relational (MongoDB) databases.
+          I write efficient REST API’s using Node.JS (primarily with TypeScript) and have a good amount of experience with relational (MySql, Postgresql) and non-relational (MongoDB) databases.
         </p>
         <p class="animate__animated animate__fadeInLeft">
           <strong>
@@ -19,12 +19,6 @@
           <ul>
             <li>
               MERN ( + HTML, CSS, JavaScript, TypeScript)
-            </li>
-            <li>
-              Python (+ Django)
-            </li>
-            <li>
-              Golang
             </li>
             <li>
               Next.js
@@ -46,108 +40,108 @@
 </template>
 
 <script>
-import NavBar from '../components/NavBar'
+import NavBar from "../components/NavBar";
 
 export default {
-  name: 'About',
+  name: "About",
   components: {
-    NavBar
-  }
-}
+    NavBar,
+  },
+};
 </script>
 
 <style scoped>
-  .parent {
-    width: 100vw;
-    position: relative;
-    height: 100vh;
+.parent {
+  width: 100vw;
+  position: relative;
+  height: 100vh;
+}
+
+.center-circle {
+  position: absolute;
+  left: 0;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 100vw;
+  height: 0;
+  padding-top: 100%;
+  background-color: #ffee51;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: all 0.6s ease;
+  cursor: pointer;
+}
+
+.container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 40%;
+  text-align: left;
+  padding: 0 100px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  box-sizing: border-box;
+  overflow: auto;
+}
+
+main {
+  width: 100%;
+  height: 60%;
+  max-width: 400px;
+  max-height: 60%;
+  overflow-y: auto;
+  overflow-x: hidden;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  box-sizing: border-box;
+}
+
+.ill-container {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 40%;
+  text-align: left;
+  padding: 0 40px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.ill-container > img {
+  width: 100%;
+  height: auto;
+}
+
+@media (max-width: 991px) {
+  .ill-container {
+    display: none;
   }
 
   .center-circle {
-    position: absolute;
-    left: 0;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    width: 100vw;
-    height: 0;
-    padding-top: 100%;
-    background-color: #FFEE51;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    transition: all 0.6s ease;
-    cursor: pointer;
+    opacity: 0.3;
   }
 
   .container {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 40%;
-    text-align: left;
-    padding: 0 100px;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    flex-direction: column;
-    box-sizing: border-box;
-    overflow: auto;
+    width: 100%;
+    justify-content: flex-start;
+    margin-top: 100px;
+    padding: 0 40px;
+    height: 130%;
   }
 
   main {
-    width: 100%;
-    height: 60%;
-    max-width: 400px;
-    max-height: 60%;
-    overflow-y: auto;
-    overflow-x: hidden;
-    justify-content: center;
-    align-items: flex-start;
-    flex-direction: column;
-    box-sizing: border-box;
-  }
-
-  .ill-container {
-    position: absolute;
-    right: 0;
-    top: 0;
-    width: 40%;
-    text-align: left;
-    padding: 0 40px;
     height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    margin-bottom: 20px;
   }
-
-  .ill-container > img {
-    width: 100%;
-    height: auto;
-  }
-
-  @media (max-width: 991px) {
-      .ill-container {
-        display: none;
-      }
-
-      .center-circle {
-        opacity: 0.3;
-      }
-
-      .container {
-        width: 100%;
-        justify-content: flex-start;
-        margin-top: 100px;
-        padding: 0 40px;
-        height: 130%;
-      }
-
-      main {
-        height: 100%;
-        margin-bottom: 20px;
-      }
-  }
+}
 </style>
